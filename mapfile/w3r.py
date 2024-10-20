d1665f84227552d9d483995be954c8a7973333b7
Matthew <matthew.marinets@gmail.com>
2024-10-20 02:36:42 -0700

    Added tooling for .w3r (regions) files

---
+++ mapfile/w3r.py
@@ -0,0 +1,108 @@
+"""
+Utilities for working with .w3r (region definition) files
+"""
+
+from dataclasses import dataclass, field, asdict
+import enum
+import tomllib
+
+from mapfile import binary
+from mapfile.util import savetext
+
+
+@dataclass
+class War3Region:
+    left: float
+    bottom: float
+    right: float
+    top: float
+    region_name: str
+    region_id: int
+    weather_effect_id: str
+    ambient_sound: str
+    colour_b: int  # u8
+    colour_g: int  # u8
+    colour_r: int  # u8
+
+
+@dataclass
+class War3RegionInfo:
+    version: int = 5
+    regions: list[War3Region] = field(default_factory=list)
+
+
+def read_w3r(raw_bytes: bytes) -> War3RegionInfo:
+    reader = binary.ByteArrayParser(raw_bytes)
+    data = War3RegionInfo(reader.read_int32())
+    assert data.version == 5, "Unknown file version"
+    num_regions = reader.read_int32()
+    for _ in range(num_regions):
+        data.regions.append(War3Region(
+            left=reader.read_float(),
+            bottom=reader.read_float(),
+            right=reader.read_float(),
+            top=reader.read_float(),
+            region_name=reader.read_cstring(),
+            region_id=reader.read_int32(),
+            weather_effect_id=reader.read_id(),
+            ambient_sound=reader.read_cstring(),
+            colour_b=reader.read_u8(),
+            colour_g=reader.read_u8(),
+            colour_r=reader.read_u8(),
+        ))
+        assert reader.read_s8() == -1
+    return data
+
+
+def to_binary(data: War3RegionInfo) -> bytes:
+    writer = binary.ByteArrayWriter()
+    writer.write_int32(data.version)
+    writer.write_int32(len(data.regions))
+    for region in data.regions:
+        writer.write_float(region.left)
+        writer.write_float(region.bottom)
+        writer.write_float(region.right)
+        writer.write_float(region.top)
+        writer.write_cstring(region.region_name)
+        writer.write_int32(region.region_id)
+        writer.write_id(region.weather_effect_id)
+        writer.write_cstring(region.ambient_sound)
+        writer.write_u8(region.colour_b)
+        writer.write_u8(region.colour_g)
+        writer.write_u8(region.colour_r)
+        writer.write_s8(-1)
+    return writer.as_bytes()
+
+
+def as_text(data: War3RegionInfo) -> str:
+    data_dict = asdict(data)
+    return savetext.to_toml(
+        data_dict,
+        ("Warcraft 3 Region file (.w3r)", "See w3r.py for type definitions",)
+    )
+
+
+def from_text(text: str) -> War3RegionInfo:
+    result = tomllib.loads(text)
+    result['regions'] = [War3Region(**region) for region in result.pop('regions')]
+    return War3RegionInfo(**result)
+
+
+if __name__ == '__main__':
+    from work import manifest
+    filenames = [f'work/{x}/war3map.w3r' for x in manifest.all_directories]
+    import os
+    os.makedirs('scratch/w3r', exist_ok=True)
+    for filename in filenames:
+        print(filename)
+        map_name = os.path.basename(os.path.dirname(filename))
+        with open(filename, 'rb') as fp2:
+            raw_data = fp2.read()
+        data = read_w3r(raw_data)
+        text = as_text(data)
+        with open(f'scratch/w3r/w3r_{map_name}.toml', 'w') as fp:
+            print(text, file=fp)
+        retrieved_data = from_text(text)
+        round_tripped = to_binary(retrieved_data)
+        assert round_tripped == raw_data
+    print('done')
