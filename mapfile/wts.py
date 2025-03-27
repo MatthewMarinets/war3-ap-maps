a4572f75cab78dc764e153ad4ba7395a68f8d168
Matthew <matthew.marinets@gmail.com>
2025-03-26 19:55:34 -0700

    mapfile: Fixing a BOM encoding issue in wts loader

--- mapfile/wts.py
+++ mapfile/wts.py
@@ -2,6 +2,9 @@
 Utilities for working with .wts (string library) files.
 """
 import re
+from codecs import BOM_UTF8
+BOM = BOM_UTF8.decode('utf-8')
+
 
 def read_wts(filename: str) -> dict[str, str]:
     """
@@ -10,6 +13,8 @@ def read_wts(filename: str) -> dict[str, str]:
     """
     with open(filename, 'r', encoding='utf-8') as file_handle:
         lines = file_handle.readlines()
+    if lines[0].startswith(BOM):
+        lines[0] = lines[0][len(BOM):]
     result: dict[str, str] = {}
     current_key = ''
     current_string: list[str] | None = None
